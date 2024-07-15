d8cc77c79d6e03c1119157daab10888cbfbfe326
zhangpan <2748879943@qq.com>
2024-07-15 18:41:07 +0800

    s

--- src/observer/storage/record/record_manager.cpp
+++ src/observer/storage/record/record_manager.cpp
@@ -507,14 +507,14 @@ RC PaxRecordPageHandler::get_chunk(Chunk &chunk)
   for(int i=0;i<chunk.column_num();i++)
   {
     int id_ = chunk.column_ids((size_t)i);
-    unique_ptr<Column> ptr();
+    unique_ptr<Column> ptr(new Column);
     char* d ;
     if(id_==0)
       d= frame_->data() + page_header_->data_offset;
       else
         d = frame_->data() + page_header_->data_offset + col_idx[id_-1];
     ptr->append(d,page_header_->record_num);
-    chunk.add_column(ptr,id_);
+    chunk.add_column(std::move(ptr),id_);
   }
   return RC::SUCCESS;
 }
